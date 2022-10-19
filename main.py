669e9378268a0b2d9f8440b2de84fa2442453fd8
JuanPabloRojasv <jprojas95395@umanizales.edu.co>
2022-10-19 18:23:11 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+main = Flask(__name__)
+
+
+
+"""
+@main.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@main.route('/calculator/add/<num1>/<num2>')
+def add(num1, num2):
+    return str(int(num1) + int(num2))
+
+
+@main.route('/calculator/subtract/<num1>/<num2>')
+def subtract(num1, num2):
+    return str(int(num1) - int(num2))
+
+
+@main.route('/calculator/multi/<num1>/<num2>')
+def multi(num1, num2):
+    return str(int(num1) * int(num2))
+
+
+@main.route('/calculator/split/<num1>/<num2>')
+def split(num1, num2):
+    return str(int(num1) / int(num2))
+
+
+@main.route('/calculator/enhance/<num1>/<num2>')
+def enhance(num1, num2):
+    return str(int(num1) ** int(num2))
+
+
+if __name__ == '__main__':
+    main.run()
+"""
+
